b85a414874860d2123d7641ec4a35d73d9302049
Gouda <mohamed8gouda@gmail.com>
2023-11-13 01:20:28 +0200

    unit 2 Lesson 3 lab Pattern 1 2 3

---
+++ C/Unit2/Labs/Pattern_123.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+//1 2 3 
+//1 2 3
+//1 2 3 
+void main ()
+{
+    
+    for (int i = 1; i <=3;i++)
+    {
+        for (int j = 1; j <= 3; j++)
+        {
+            /* code */
+            printf("%d ",j);
+        }
+        printf("\n");
+    }
+}
