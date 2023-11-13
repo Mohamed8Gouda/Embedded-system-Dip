55ea0b9eaa913fbf094fd1c64cbabe804682ae12
Gouda <mohamed8gouda@gmail.com>
2023-11-13 05:26:57 +0200

    Assignment 1 Example 7 without TEMP

---
+++ C_Programming/Unit2/Assignment 1/EX 7/Swap_Two_Numbers.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+/*  
+
+    #########Console_output######
+    Enter value of a: 1.20
+    Enter value of b: 2.45
+
+    After swapping, value of a = 2.45
+    After swapping, value of b = 1.2
+    #############################
+ */
+int main ()
+{
+    float NumberA = 0;
+    float NumberB = 0; 
+    printf("Enter value of a: ");
+    scanf("%f", &NumberA);
+    printf("Enter value of b: ");
+    scanf("%f", &NumberB);
+    // Logic 
+    NumberA += NumberB;
+    NumberB  = NumberA - NumberB ;
+    NumberA  = NumberA - NumberB ;
+    printf("After swapping, value of a = %0.2f\n",NumberA);
+    printf("After swapping, value of b = %0.2f\n",NumberB);    
+    return 0;
+}
---
+++ C_Programming/Unit2/Assignment 1/EX 7/readme.txt
@@ -0,0 +1,11 @@
+EX7:
+
+Write Source Code to Swap Two Numbers without Temp 
+
+#########Console_output######
+Enter value of a: 1.20
+Enter value of b: 2.45
+
+After swapping, value of a = 2.45
+After swapping, value of b = 1.2
+#############################
