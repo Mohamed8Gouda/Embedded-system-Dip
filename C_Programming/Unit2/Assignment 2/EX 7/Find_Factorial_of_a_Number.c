42b1169344cf33fd631636bad0a80f8ce9b43be7
Gouda <mohamed8gouda@gmail.com>
2023-11-14 22:17:38 +0200

    Assignment 2 Example 7

---
+++ C_Programming/Unit2/Assignment 2/EX 7/Find_Factorial_of_a_Number.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+/* 
+    ##########Console-output###
+    #Output 1
+    Enter an integer: -5
+    Error!!! Factorial of negative number doesn't exist.
+    #Output 2
+    Enter an integer: 10
+    Factorial = 3628800
+    ###########################
+*/
+int main ()
+{
+    char Number = 0 ; 
+    unsigned int Factorial = 1;
+    
+    //Data input 
+    printf("Enter an integer: "); 
+    scanf ("%d",&Number);
+    
+    //Logic
+    if (Number>1)
+    {
+        for (int Counter = Number ; Counter > 1; Counter-- )
+        {
+            Factorial *= Counter ;
+        }
+
+        printf ("Factorial = %d",Factorial);
+    }
+    else if (Number==0 || Number ==1)
+    {
+        printf ("Factorial = 1");
+    }
+    else
+    {
+        printf("Error!!! Factorial of negative number doesn't exist.");
+    }
+
+    return 0;
+}
---
+++ C_Programming/Unit2/Assignment 2/EX 7/readme.txt
@@ -0,0 +1,13 @@
+EX1:
+
+Write C Program to Calculate Find Factorial of a Number
+
+i should see the Console as following:
+##########Console-output###
+#Output 1
+Enter an integer: -5
+Error!!! Factorial of negative number doesn't exist.
+#Output 2
+Enter an integer: 10
+Factorial = 3628800
+###########################
