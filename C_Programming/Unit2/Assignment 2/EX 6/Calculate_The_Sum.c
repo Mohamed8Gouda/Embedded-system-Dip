513bfd7617cca755af4459e062bd0e852d173186
Gouda <mohamed8gouda@gmail.com>
2023-11-14 21:29:08 +0200

    Assginment 2 Example 6

---
+++ C_Programming/Unit2/Assignment 2/EX 6/Calculate_The_Sum.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+/* 
+    ##########Console-output###
+    #Output 
+    Enter an integer: 100
+    sum = 5050
+    ###########################
+*/
+int main ()
+{
+    unsigned char Number = 0 ; 
+    unsigned int Sum = 0;
+    
+    //Data input 
+    printf("Enter an integer: "); 
+    scanf ("%d",&Number);
+    
+    //Logic
+    for (int Counter = 0 ; Counter <= Number; Counter++ )
+    {
+        Sum += Counter ;
+    }
+
+    printf ("sum = %d",Sum);
+
+    return 0;
+}
---
+++ C_Programming/Unit2/Assignment 2/EX 6/readme.txt
@@ -0,0 +1,10 @@
+EX1:
+
+Write C Program to Calculate Sum of Natural Numbers
+
+i should see the Console as following:
+##########Console-output###
+#Output 
+Enter an integer: 100
+sum = 5050
+###########################
