c0c43d13f87730fc5d1c835e74a3f370d190a5aa
Gouda <mohamed8gouda@gmail.com>
2023-11-14 08:17:12 +0200

    Assignmaent 2 Example 2

---
+++ C_Programming/Unit2/Assignment 2/EX 2/Check_Vowel_or_Consonant.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+/* 
+    ##########Console-output###
+    #Output 1 
+    Enter an alaphbet: i
+    i is a vowel.
+    #Output 2
+    Enter an alaphbet: G 
+    G is a consonant.
+    ###########################
+*/
+int main ()
+{
+    signed char Character = 0 ;
+    char condetion=0; 
+    printf("Enter an alaphbet: "); 
+    scanf ("%c",&Character);
+    condetion = (Character=='a'||Character=='A'||Character=='e'||Character=='E'||Character=='o'||Character=='O'||Character=='i'||Character=='I'||Character=='u'||Character=='U');
+    if (condetion)
+    {
+        printf("%c is a vowel.",Character);
+    }
+    else 
+    {
+        printf("%c is a consonant.",Character);       
+    }
+    return 0;
+}
---
+++ C_Programming/Unit2/Assignment 2/EX 2/readme.txt
@@ -0,0 +1,12 @@
+EX2:
+
+Write C Program to Check if the charcater is vowel or consonant
+i should see the Console as following:
+##########Console-output###
+#Output 1 
+Enter an alaphbet: i
+i is a vowel.
+#Output 2
+Enter an alaphbet: G 
+G is a consonant.
+###########################
